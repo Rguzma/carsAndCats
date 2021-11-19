c7fcee5f9e2563abdc275f4b6ef76f184765b5b8
Your Name <you@example.com>
2021-11-18 22:02:24 -0600

    rguzma

---
+++ app.js
@@ -0,0 +1,77 @@
+const http = require('http');
+const fs = require('fs');
+const server = http.createServer(function (request, response){
+    console.log('client request URL: ', request.url);
+    
+    // this is how we do routing:
+    if(request.url === '/cars') {
+        fs.readFile('./views/cars.html',  function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    }
+    else if(request.url === '/cats') {
+        fs.readFile('./views/cats.html', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    }
+    else if (request.url === '/cars/new') {
+        fs.readFile('./views/newCar.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    }
+    else if(request.url === '/stylesheets/style.css'){
+        fs.readFile('./stylesheets/style.css', 'utf8', function(errors, contents){
+         response.writeHead(200, {'Content-type': 'text/css'});
+         response.write(contents);
+         response.end();
+        })
+      }
+      else if(request.url === '/images/cats1.jpg'){
+        // notice we won't include the utf8 encoding
+        fs.readFile('./images/cats1.jpg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        })
+      }
+      else if(request.url === '/images/cats2.jpg'){
+        // notice we won't include the utf8 encoding
+        fs.readFile('./images/cats2.jpg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        })
+      }
+      else if(request.url === '/images/cars1.jpg'){
+        // notice we won't include the utf8 encoding
+        fs.readFile('./images/cars1.jpg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        })
+      }
+      else if(request.url === '/images/cars2.jpg'){
+        // notice we won't include the utf8 encoding
+        fs.readFile('./images/cars2.jpg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        })
+      }
+    // request didn't match anything:
+    else {
+        response.writeHead(404);
+        response.end('File not found!!!');
+    }
+});
+// tell your server which port to run on
+server.listen(7077);
+// server.listen(6789);
+// print to terminal window
+console.log("Running in localhost at port 7077");
